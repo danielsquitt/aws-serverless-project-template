a42bf949c8fbaa7a8a6c2d3b31c25843b32a5a73
Daniel Squittieri <danielsquitt@gmail.com>
2022-07-30 17:47:10 +0200

    Updated script to create git repositories

--- scripts.sh
+++ scripts.sh
@@ -2,7 +2,7 @@ set -e
 
 ## Deploy base services
 echo "Deploying base services"
-## sls deploy --config base.services.template.yml --verbose --param="app=tbb"
+sls deploy --config base.services.template.yml --verbose --param="app=tbb"
 rm -rf ./.serverless
 
 APP_NAME=$(aws cloudformation describe-stacks --stack-name tbb-base-services --query "Stacks[*].Outputs[?OutputKey=='ApplicationName'].OutputValue" --output text)
@@ -24,72 +24,73 @@ echo "SERV_REPO = $SERV_REPO // Services git repository"
 # Deploy Backend CICD
 LAYER="backend"
 echo "Deploying backend cicd"
-## sls deploy --config cicd.template.yml --verbose \
-##     --param="layer=$LAYER"  \
-##     --param="app=$APP_NAME"  \
-##     --param="bucket=$S3_BUCKET" \
-##     --param="repo=$BACK_REPO"  \
-##     --param="roleArn=$CICD_ROLE" 
+sls deploy --config cicd.template.yml --verbose \
+    --param="layer=$LAYER"  \
+    --param="app=$APP_NAME"  \
+    --param="bucket=$S3_BUCKET" \
+    --param="repo=$BACK_REPO"  \
+    --param="roleArn=$CICD_ROLE" 
 rm -rf ./.serverless
 echo "Backend cicd deployed succesfull"
 
 # Deploy Front CICD
 LAYER="frontend"
 echo "Deploying frontend cicd"
-## sls deploy --config cicd.template.yml --verbose \
-##     --param="layer=$LAYER"  \
-##     --param="app=$APP_NAME"  \
-##     --param="bucket=$S3_BUCKET" \
-##     --param="repo=$FRONT_REPO"  \
-##     --param="roleArn=$CICD_ROLE" 
+sls deploy --config cicd.template.yml --verbose \
+    --param="layer=$LAYER"  \
+    --param="app=$APP_NAME"  \
+    --param="bucket=$S3_BUCKET" \
+    --param="repo=$FRONT_REPO"  \
+    --param="roleArn=$CICD_ROLE" 
 rm -rf ./.serverless
 echo "Frontend cicd deployed succesfull"
 
 # Deploy Services CICD
 LAYER="services"
 echo "Deploying services"
-##sls deploy --config cicd.template.yml --verbose \
-##    --param="layer=$LAYER"  \
-##    --param="app=$APP_NAME"  \
-##    --param="bucket=$S3_BUCKET" \
-##    --param="repo=$SERV_REPO"  \
-##    --param="roleArn=$CICD_ROLE" 
+sls deploy --config cicd.template.yml --verbose \
+   --param="layer=$LAYER"  \
+   --param="app=$APP_NAME"  \
+   --param="bucket=$S3_BUCKET" \
+   --param="repo=$SERV_REPO"  \
+   --param="roleArn=$CICD_ROLE" 
 rm -rf ./.serverless
 echo "Services cicd deployed succesfull"
 
 # Create backend repository folder
-mkdir -p ../backend
-mkdir -p ../frontend
-mkdir -p ../services
-
-# Copy base files
-cp -a base/. ../backend/
-cp -a base/. ../frontend/
-cp -a base/. ../frontend/
 
 # Init backend git proyect
-cd ../backend
-git init
+cd ..
+git clone --origin aws ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/$BACK_REPO
+cp -a templates/base/. $BACK_REPO
+cd $BACK_REPO
 git checkout -b dev
 git add .
 git commit -m 'Initial Commit'
 git checkout -b master
-git merge dev
+git push --set-upstream aws dev
+git push --set-upstream aws master
 
-# Init backend git proyect
-cd ../frontend
-git init
+# Init frontend git proyect
+cd ..
+git clone --origin aws ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/$FRONT_REPO
+cp -a templates/base/. $FRONT_REPO
+cd $FRONT_REPO
 git checkout -b dev
 git add .
 git commit -m 'Initial Commit'
 git checkout -b master
-git merge dev
+git push --set-upstream aws dev
+git push --set-upstream aws master
 
-# Init backend git proyect
-cd ../services
-git init
+# Init service git proyect
+cd ..
+git clone --origin aws ssh://git-codecommit.eu-west-1.amazonaws.com/v1/repos/$SERV_REPO
+cp -a templates/base/. $SERV_REPO
+cd $SERV_REPO
 git checkout -b dev
 git add .
 git commit -m 'Initial Commit'
 git checkout -b master
-git merge dev
+git push --set-upstream aws dev
+git push --set-upstream aws master
